30a0ae9d3b1c6b6260c8f05fb62a0184f57e3e79
Grant Gumina <grantgumina@outlook.com>
2016-05-21 09:56:15 -0700

    adding scheduled message count function

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,22 @@
 
-Parse.Cloud.define('hello', function(req, res) {
+Parse.Cloud.define('hello', function (req, res) {
   res.success('Hi');
+});
+
+Parse.Cloud.define('getNumberOfScheduledMessagesThisYear', function (req, res) {
+	var smQuery = new Parse.Query("ScheduledMessage");
+
+	var subDate = req.user.get("subscriptionDate");
+	if (subDate != null) {
+		var maxDate = subDate;
+		maxDate.setYear(subDate.getMonth() + 12);
+		smQuery.greaterThanOrEqualTo("createdAt", subDate);
+		smQuery.lessThanOrEqualTo("createdAt", maxDate);
+	}
+
+	return smQuery.find().then(function (scheduledMessages) {
+		return res.success(scheduledMessages.length());
+	}, function (error) {
+		return res.error(error);
+	});
 });
